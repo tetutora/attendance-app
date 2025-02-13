479e65a38bccfe0f3b0709966cc9b978b187da19
kitano <beiyelinai01@gmail.com>
2025-02-13 16:47:30 +0900

    会員登録処理

--- src/app/Http/Controllers/RegisterController.php
+++ src/app/Http/Controllers/RegisterController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
     public function showRegisterForm()
     {
         return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $user = $request->all();
+        User::create($user);
     }
 }
