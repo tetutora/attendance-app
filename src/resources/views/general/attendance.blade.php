59ce8d24b88cd8810fea03f2f3d1ff68746eb095
kitano <beiyelinai01@gmail.com>
2025-02-14 16:40:14 +0900

    勤怠登録実装

--- src/resources/views/general/attendance.blade.php
+++ src/resources/views/general/attendance.blade.php
@@ -21,46 +21,60 @@
 @endphp
 
 <div class="attendance-container">
-    @if(!$attendance)
-        <div class="status-tag">勤務外</div>
-        <form action="{{ route('general.attendance') }}" method="post">
-            @csrf
-            <button type="submit" class="attendance-button">出勤</button>
-        </form>
-    @elseif($attendance && is_null($attendance->clock_out))
-        <div class="status-tag">
-            @if(is_null($attendance->break_start))
-                出勤中
-            @elseif(!is_null($attendance->break_start) && is_null($attendance->break_end))
-                休憩中
-            @else
-                出勤中
+    <div class="status-tag">
+        @if($attendance)
+            @if(is_null($attendance->clock_out))
+                @if(is_null($attendance->break_start))
+                    出勤中
+                @elseif(!is_null($attendance->break_start) && is_null($attendance->break_end))
+                    休憩中
+                @else
+                    出勤中
+                @endif
+            @elseif($attendance->clock_out)
+                退勤済
             @endif
-        </div>
-        @if(is_null($attendance->break_start) || !is_null($attendance->break_end))
-            <form action="{{ route('general.attendance') }}" method="post">
-                @csrf
-                <button type="submit" class="attendance-button" name="action" value="clock_out">退勤</button>
-            </form>
+        @else
+            勤務外
         @endif
-        @if(is_null($attendance->break_start) || !is_null($attendance->break_end))
-            <form action="{{ route('general.attendance') }}" method="post">
-                @csrf
-                <button type="submit" class="attendance-button" name="action" value="break_start">休憩入</button>
-            </form>
-        @elseif(!is_null($attendance->break_start) && is_null($attendance->break_end))
-            <form action="{{ route('general.attendance') }}" method="post">
-                @csrf
-                <button type="submit" class="attendance-button" name="action" value="break_end">休憩戻</button>
-            </form>
-        @endif
-    @elseif($attendance && $attendance->clock_out)
-        <div class="status-tag">退勤済</div>
-        <p>お疲れ様でした。</p>
-    @endif
+    </div>
+
     <div class="date">{{ $date }} {{ $dayOfWeek }}</div>
     <div class="time" id="current-time">{{ now()->format('H:i:s') }}</div>
+
+    <div class="button-container">
+        @if($attendance)
+            @if(is_null($attendance->clock_out))
+                @if(is_null($attendance->break_start) || !is_null($attendance->break_end))
+                    <form action="{{ route('general.attendance') }}" method="post">
+                        @csrf
+                        <button type="submit" class="attendance-button clock-out" name="action" value="clock_out">退勤</button>
+                    </form>
+                @endif
+
+                @if(is_null($attendance->break_start) || !is_null($attendance->break_end))
+                    <form action="{{ route('general.attendance') }}" method="post">
+                        @csrf
+                        <button type="submit" class="attendance-button break-start" name="action" value="break_start">休憩入</button>
+                    </form>
+                @elseif(!is_null($attendance->break_start) && is_null($attendance->break_end))
+                    <form action="{{ route('general.attendance') }}" method="post">
+                        @csrf
+                        <button type="submit" class="attendance-button break-end" name="action" value="break_end">休憩戻</button>
+                    </form>
+                @endif
+            @else
+                <p>お疲れ様でした。</p>
+            @endif
+        @else
+            <form action="{{ route('general.attendance') }}" method="post">
+                @csrf
+                <button type="submit" class="attendance-button clock-in">出勤</button>
+            </form>
+        @endif
+    </div>
 </div>
+
 @endsection
 
 @section('script')
