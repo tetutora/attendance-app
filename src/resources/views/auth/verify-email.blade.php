30d0d71ea980b9edd1b37f612599309ac6c74660
kitano <beiyelinai01@gmail.com>
2025-03-13 14:11:29 +0900

    RegisterTest実装

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -1,28 +1,35 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/verify-email.css') }}">
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('メールアドレスの確認') }}</div>
-
-                    <div class="card-body">
+                    <div class="card-body text-center">
                         @if (session('resent'))
-                            <div class="alert alert-success" role="alert">
+                            <div class="alert alert-success bold-text" role="alert">
                                 {{ __('新しい確認リンクがあなたのメールアドレスに送信されました。') }}
                             </div>
                         @endif
 
-                        {{ __('続行する前に、確認リンクが含まれたメールを確認してください。') }}
-                        {{ __('メールが届いていない場合') }},
-                        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                            @csrf
-                            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('もう一度確認メールを送信する') }}</button>.
-                        </form>
+                        <p class="bold-text">{{ __('登録していただいたメールアドレスに認証メールを送付しました。') }}</p>
+                        <p class="bold-text">{{ __('メール認証を完了してください。') }}</p>
 
-                        <div class="mt-3">
-                            <a href="https://mailtrap.io/inboxes" class="btn btn-primary">{{ __('Mailtrapに移動') }}</a>
+                        <div class="button-container">
+                            <a href="https://mailtrap.io" class="custom-button">{{ __('認証はこちらから') }}</a>
+                        </div>
+
+                        <div class="resend-link-container">
+                            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                                @csrf
+                                <button type="submit" class="resend-link">
+                                    {{ __('認証メールを再送する') }}
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
